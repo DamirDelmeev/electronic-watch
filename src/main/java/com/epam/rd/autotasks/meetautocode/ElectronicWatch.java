962a42024a4104c9382409dac3d118208ead21fd
Damir Delmeev <216345i@mail.ru>
2021-09-29 00:38:19 +0300

    Return the electronic watch

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -1,11 +1,20 @@
 package com.epam.rd.autotasks.meetautocode;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        System.out.println(someMeth(seconds));
+
+
+    }
+    static public String someMeth(Integer second){
+        LocalTime localDateTime= LocalTime.of(0,0,0);
+        return localDateTime.plusSeconds(second).format(DateTimeFormatter.ofPattern("H:mm:ss"));
 
 
     }
